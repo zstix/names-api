371500b17e4b4317d2db896bd81ba5abe9ff782f
Zack Stickles <zstickles@gmail.com>
2021-01-22 20:24:16 -0800

    chore: repository setup

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const PORT = 1234;
 
 const app = express();
 
-app.get('/', (_req, res) => res.send('Express + Typescript !!'));
+app.get('/', (_req, res) => res.send('Express + Typescript'));
 
 app.listen(PORT, () => {
   console.log(`[server] Server is running at https://localhost:${PORT}`);
