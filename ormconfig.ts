169fc6798d82f7e8baa007b5bacbc06027e75814
Zack Stickles <zstickles@newrelic.com>
2021-01-25 09:33:26 -0800

    chore: temporary skipping ssl rejections

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,15 +1,19 @@
 module.exports = {
-  "type": "postgres",
-  "host": process.env.TYPEORM_HOST,
-  "port": 5432,
-  "username": process.env.TYPEORM_USERNAME,
-  "password": process.env.TYPEORM_PASSWORD,
-  "database": process.env.TYPEORM_DATABASE,
-  "synchronize": false,
-  "logging": true,
-  "entities": ["src/**/*.entity.ts"],
-  "migrations": ["src/migrations/*.ts"],
-  "cli": {
-    "migrationsDir": "src/migrations"
-  }
+  type: "postgres",
+  host: process.env.TYPEORM_HOST,
+  port: 5432,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: false,
+  logging: true,
+  entities: ["src/**/*.entity.ts"],
+  migrations: ["src/migrations/*.ts"],
+  cli: {
+    migrationsDir: "src/migrations",
+  },
+  ssl: {
+    // TODO: remove this after setting up ca / ssl
+    rejectUnauthorized: false,
+  },
 };
