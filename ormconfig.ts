d47136188b73a1ea14588ac149c19dc7a03ade4a
Zack Stickles <zstickles@newrelic.com>
2021-01-25 16:05:53 -0800

    chore: configure port

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from "typeorm";
 const options: ConnectionOptions = {
   type: "postgres",
   host: process.env.TYPEORM_HOST,
-  port: 5432,
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5432,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
