6bbe501ba9423ee278bea0c2f6ceb4678225e39d
Zack Stickles <zstickles@newrelic.com>
2021-01-25 16:07:05 -0800

    chore: added top-level route

--- app.ts
+++ app.ts
@@ -1,25 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { createConnection } from 'typeorm';
-import NamesController from './src/name/name.controller';
+import express from "express";
+import dotenv from "dotenv";
+import { createConnection } from "typeorm";
+import NamesController from "./src/name/name.controller";
 
 dotenv.config();
 
 const port = 1337;
 const app = express();
 
-createConnection().then((conn) => {
-  console.log('Connected to database');
+createConnection()
+  .then((conn) => {
+    console.log("Connected to database");
 
-  app.get('/ping', (_req, res) => res.send('pong'));
+    app.get("/", (_req, res) => res.send("Names API"));
 
-  app.get('/names', NamesController.getAll(conn)); // TODO: pagination
-  app.get('/names/:id', NamesController.getOne(conn));
+    app.get("/names", NamesController.getAll(conn)); // TODO: pagination
+    app.get("/names/:id", NamesController.getOne(conn));
 
-  app.listen(port, () => {
-    console.log(`Server is running at https://localhost:${port}`);
+    app.listen(port, () => {
+      console.log(`Server is running at https://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("[!] Unable to connect to the database");
+    console.log(error);
   });
-}).catch((error) => {
-  console.log('[!] Unable to connect to the database');
-  console.log(error);
-});
