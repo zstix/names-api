2c76b50562d48814e67e8a4b8ed6b85a9c9a9501
Zack Stickles <zstickles@newrelic.com>
2021-01-25 16:32:08 -0800

    chore: formatting updates to trigger a build

--- src/migrations/1611531698344-createNameTable.ts
+++ src/migrations/1611531698344-createNameTable.ts
@@ -1,30 +1,31 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createNameTable1611531698344 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.createTable(new Table({
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
         name: "names",
         columns: [
           {
-            name: 'id',
-            type: 'int',
+            name: "id",
+            type: "int",
             isPrimary: true,
             isGenerated: true,
-            generationStrategy: 'increment'
+            generationStrategy: "increment",
           },
           {
-            name: 'name',
-            type: 'varchar',
+            name: "name",
+            type: "varchar",
             isUnique: true,
-            isNullable: false
-          }
-        ]
-      }), false);
-    }
+            isNullable: false,
+          },
+        ],
+      }),
+      false
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropTable('names');
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("names");
+  }
 }
